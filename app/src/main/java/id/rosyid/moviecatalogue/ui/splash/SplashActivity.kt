b18f2bc43358bf74eb403c101c81448f6882105f
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-04-24 15:51:44 +0700

    create splash activity

---
+++ app/src/main/java/id/rosyid/moviecatalogue/ui/splash/SplashActivity.kt
@@ -0,0 +1,41 @@
+package id.rosyid.moviecatalogue.ui.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import id.rosyid.moviecatalogue.databinding.ActivitySplashBinding
+import id.rosyid.moviecatalogue.utils.Fullscreen
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // TODO: Create Homepage Activity
+        val intentHomepage = Intent(this, HomepageActivity::class.java)
+        lifecycleScope.launch(Dispatchers.IO) {
+            delay(TIMEOUT)
+            withContext(Dispatchers.Main) {
+                startActivity(intentHomepage)
+                finish()
+            }
+        }
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) Fullscreen.apply(window)
+    }
+
+    companion object {
+        private const val TIMEOUT = 3000L
+    }
+}
---
+++ app/src/main/java/id/rosyid/moviecatalogue/utils/ChangeScreenWindow.kt
@@ -0,0 +1,29 @@
+package id.rosyid.moviecatalogue.utils
+
+import android.os.Build
+import android.view.View
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+
+object Fullscreen {
+    fun apply(window: Window) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.let {
+                it.hide(WindowInsets.Type.statusBars() or WindowInsets.Type.navigationBars())
+                it.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                )
+        }
+    }
+}
