aeb14104ae762a26b0b1c6ed45e1641ef689b9a2
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-11 09:19:29 +0700

    change constanta value
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/id/rosyid/moviecatalogue/data/remote/api/TMdbService.kt
+++ app/src/main/java/id/rosyid/moviecatalogue/data/remote/api/TMdbService.kt
@@ -13,7 +13,7 @@ interface TMdbService {
     companion object {
         const val BASE_URL = "https://api.themoviedb.org/3/"
         const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/"
-        const val API_KEY = BuildConfig.TMDB_API_KEY
+        const val API_KEY = BuildConfig.API_KEY
     }
 
     @GET("discover/movie")
