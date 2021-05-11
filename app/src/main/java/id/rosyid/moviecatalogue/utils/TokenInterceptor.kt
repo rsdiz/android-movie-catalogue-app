27068f8dca54dd1fc1daecf2ad25ccd1e825ac70
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-11 09:12:51 +0700

    reformat code
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/id/rosyid/moviecatalogue/utils/TokenInterceptor.kt
+++ app/src/main/java/id/rosyid/moviecatalogue/utils/TokenInterceptor.kt
@@ -3,10 +3,10 @@ package id.rosyid.moviecatalogue.utils
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class TokenInterceptor(private val apiKey: String): Interceptor {
+class TokenInterceptor(private val apiKey: String) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var original = chain.request()
-        val url = original.url().newBuilder().addQueryParameter("api_key", apiKey).build()
+        val url = original.url.newBuilder().addQueryParameter("api_key", apiKey).build()
         original = original.newBuilder().url(url).build()
         return chain.proceed(original)
     }
