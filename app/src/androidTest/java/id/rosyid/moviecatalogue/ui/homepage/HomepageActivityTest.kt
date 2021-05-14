982329f9da1eeabebe49f377bf798e6f80603fc3
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-14 08:49:10 +0700

    update instrumental testing
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/androidTest/java/id/rosyid/moviecatalogue/ui/homepage/HomepageActivityTest.kt
+++ app/src/androidTest/java/id/rosyid/moviecatalogue/ui/homepage/HomepageActivityTest.kt
@@ -1,77 +1,67 @@
 package id.rosyid.moviecatalogue.ui.homepage
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.NoMatchingViewException
-import androidx.test.espresso.ViewAssertion
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.contrib.RecyclerViewActions.scrollToPosition
 import androidx.test.espresso.matcher.ViewMatchers.*
 import id.rosyid.moviecatalogue.R
-import id.rosyid.moviecatalogue.data.MovieEntity
-import id.rosyid.moviecatalogue.data.TvEntity
 import id.rosyid.moviecatalogue.ui.splash.SplashActivity
-import id.rosyid.moviecatalogue.utils.FormatPattern
-import id.rosyid.moviecatalogue.utils.toStringWithPattern
-import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.equalTo
+import id.rosyid.moviecatalogue.utils.EspressoIdlingResource
+import org.junit.After
 import org.junit.Before
 import org.junit.Test
 
 class HomepageActivityTest {
-    private val dummyMovies = listOf<MovieEntity>()
-    private val dummyTvSeries = listOf<TvEntity>()
-
     @Before
     fun setUp() {
         ActivityScenario.launch(SplashActivity::class.java)
         ActivityScenario.launch(HomepageActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
     }
 
     @Test
     fun loadMovies() {
         onView(withId(R.id.rv_movies)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_movies)).perform(scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.size))
+        onView(withId(R.id.rv_movies)).perform(scrollToPosition<RecyclerView.ViewHolder>(20))
     }
 
     @Test
-    fun loadMovieDetail() {
+    fun loadDetailMovie() {
         onView(withId(R.id.rv_movies)).perform(
             actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
+        onView(withId(R.id.banner_image)).check(matches(isDisplayed()))
         onView(withId(R.id.content_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.content_title)).check(matches(withText(dummyMovies[0].title)))
         onView(withId(R.id.content_release_date)).check(matches(isDisplayed()))
-        onView(withId(R.id.content_release_date)).check(
-            matches(
-                withText(
-                    dummyMovies[0].releaseDate.toStringWithPattern(
-                        FormatPattern.DEFAULT_PATTERN
-                    )
-                )
-            )
-        )
         onView(withId(R.id.content_poster)).check(matches(isDisplayed()))
-        onView(withId(R.id.content_poster)).check(matches(withTagValue(equalTo(dummyMovies[0].poster))))
-        onView(withId(R.id.rv_keywords)).check(RecyclerViewItemCountAssertion(dummyMovies[0].keywords.size))
+        onView(withId(R.id.content_tagline)).check(matches(isDisplayed()))
+        onView(withId(R.id.content_overview)).check(matches(isDisplayed()))
     }
 
     @Test
     fun loadTvSeries() {
         onView(withText("TV SHOWS")).perform(click())
         onView(withId(R.id.rv_tv_series)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_tv_series)).perform(scrollToPosition<RecyclerView.ViewHolder>(dummyMovies.size))
+        onView(withId(R.id.rv_tv_series)).perform(
+            scrollToPosition<RecyclerView.ViewHolder>(20)
+        )
     }
 
     @Test
-    fun loadTvSeriesDetail() {
+    fun loadDetailTvSeries() {
         onView(withText("TV SHOWS")).perform(click())
         onView(withId(R.id.rv_tv_series)).perform(
             actionOnItemAtPosition<RecyclerView.ViewHolder>(
@@ -79,21 +69,11 @@ class HomepageActivityTest {
                 click()
             )
         )
+        onView(withId(R.id.banner_image)).check(matches(isDisplayed()))
         onView(withId(R.id.content_title)).check(matches(isDisplayed()))
-        onView(withId(R.id.content_title)).check(matches(withText(dummyTvSeries[0].title)))
+        onView(withId(R.id.content_release_date)).check(matches(isDisplayed()))
         onView(withId(R.id.content_poster)).check(matches(isDisplayed()))
-        onView(withId(R.id.content_poster)).check(matches(withTagValue(equalTo(dummyTvSeries[0].poster))))
-        onView(withId(R.id.rv_keywords)).check(RecyclerViewItemCountAssertion(dummyTvSeries[0].keywords.size))
-    }
-}
-
-class RecyclerViewItemCountAssertion(private val expectedCount: Int) : ViewAssertion {
-    override fun check(view: View, noViewFoundException: NoMatchingViewException?) {
-        if (noViewFoundException != null) {
-            throw noViewFoundException
-        }
-        val recyclerView = view as RecyclerView
-        val adapter = recyclerView.adapter
-        assertThat(adapter!!.itemCount, `is`(expectedCount))
+        onView(withId(R.id.content_tagline)).check(matches(isDisplayed()))
+        onView(withId(R.id.content_overview)).check(matches(isDisplayed()))
     }
 }
