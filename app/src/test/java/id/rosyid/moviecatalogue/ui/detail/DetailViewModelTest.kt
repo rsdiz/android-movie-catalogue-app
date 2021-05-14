c0fcd6c0dffe3f6ac5f306346d40f4d41e18b51e
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-14 08:48:38 +0700

    create unit testing for detail movies and tv series view model
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/test/java/id/rosyid/moviecatalogue/ui/detail/DetailViewModelTest.kt
+++ app/src/test/java/id/rosyid/moviecatalogue/ui/detail/DetailViewModelTest.kt
@@ -1,60 +1,88 @@
 package id.rosyid.moviecatalogue.ui.detail
 
-import id.rosyid.moviecatalogue.data.MovieEntity
-import id.rosyid.moviecatalogue.data.TvEntity
-import org.junit.Assert.*
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.verify
+import id.rosyid.moviecatalogue.data.entities.MovieDetail
+import id.rosyid.moviecatalogue.data.entities.TvDetail
+import id.rosyid.moviecatalogue.data.repository.MoviesRepository
+import id.rosyid.moviecatalogue.data.repository.TvRepository
+import id.rosyid.moviecatalogue.utils.DataDummy
+import id.rosyid.moviecatalogue.utils.Resource
+import id.rosyid.moviecatalogue.utils.TestCoroutineRule
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Assert
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
 
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class DetailViewModelTest {
-    private lateinit var detailViewModel: DetailViewModel
-    private lateinit var dummyMovies: MovieEntity
-    private val movieId = dummyMovies.movieId
-    private lateinit var dummyTvSeries: TvEntity
-    private val tvId = dummyTvSeries.tvId
+    private lateinit var viewModel: DetailViewModel
+
+    @Mock
+    private lateinit var moviesRepository: MoviesRepository
+
+    @Mock
+    private lateinit var tvsRepository: TvRepository
+
+    @Mock
+    private lateinit var observerTvDetail: Observer<Resource<TvDetail>>
+
+    @Mock
+    private lateinit var observerMovieDetail: Observer<Resource<MovieDetail>>
+
+    @get:Rule
+    val instantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
 
     @Before
     fun setUp() {
-        detailViewModel = DetailViewModel()
+        viewModel = DetailViewModel(tvsRepository, moviesRepository)
     }
 
     @Test
-    fun getMoviesDetail() {
-        detailViewModel.setSelectedId(movieId, DetailActivity.TYPE_MOVIES)
-        val selectedMovie: MovieEntity = detailViewModel.getItemDetail() as MovieEntity
-        assertNotNull(selectedMovie)
-        assertEquals(dummyMovies.movieId, selectedMovie.movieId)
-        assertEquals(dummyMovies.releaseDate, selectedMovie.releaseDate)
-        assertEquals(dummyMovies.title, selectedMovie.title)
-        assertEquals(dummyMovies.runtime, selectedMovie.runtime)
-        assertEquals(dummyMovies.poster, selectedMovie.poster)
-        assertEquals(dummyMovies.overview, selectedMovie.overview)
-        assertEquals(dummyMovies.status, selectedMovie.status)
-        assertEquals(dummyMovies.originalLanguage, selectedMovie.originalLanguage)
-        assertEquals(dummyMovies.userScore, selectedMovie.userScore)
-        assertEquals(dummyMovies.genres, selectedMovie.genres)
-        assertEquals(dummyMovies.keywords, selectedMovie.keywords)
-        assertEquals(dummyMovies.credits, selectedMovie.credits)
-        assertEquals(dummyMovies.socialLinks, selectedMovie.socialLinks)
+    fun getTvDetail() = testCoroutineRule.runBlockingTest {
+        val idTv = DataDummy.getTvDetail().id
+        val resource = Resource.success(DataDummy.getTvDetail())
+        val data = MutableLiveData<Resource<TvDetail>>().apply { value = resource }
+        `when`(tvsRepository.getTvDetailById(idTv)).thenReturn(data)
+
+        viewModel.initDetail(idTv, DetailActivity.TYPE_TVSERIES)
+        viewModel.tvDetail.observeForever(observerTvDetail)
+        verify(tvsRepository).getTvDetailById(idTv)
+        verify(observerTvDetail).onChanged(resource)
+        viewModel.tvDetail.removeObserver(observerTvDetail)
+
+        val tvDetail = viewModel.tvDetail.value
+        Assert.assertNotNull(tvDetail)
+        Assert.assertEquals(resource.data, tvDetail?.data)
     }
 
     @Test
-    fun getTvSeriesDetail() {
-        detailViewModel.setSelectedId(tvId, DetailActivity.TYPE_TVSERIES)
-        val selectedTvSeries: TvEntity = detailViewModel.getItemDetail() as TvEntity
-        assertNotNull(selectedTvSeries)
-        assertEquals(dummyTvSeries.tvId, selectedTvSeries.tvId)
-        assertEquals(dummyTvSeries.type, selectedTvSeries.type)
-        assertEquals(dummyTvSeries.title, selectedTvSeries.title)
-        assertEquals(dummyTvSeries.runtime, selectedTvSeries.runtime)
-        assertEquals(dummyTvSeries.poster, selectedTvSeries.poster)
-        assertEquals(dummyTvSeries.overview, selectedTvSeries.overview)
-        assertEquals(dummyTvSeries.status, selectedTvSeries.status)
-        assertEquals(dummyTvSeries.originalLanguage, selectedTvSeries.originalLanguage)
-        assertEquals(dummyTvSeries.userScore, selectedTvSeries.userScore)
-        assertEquals(dummyTvSeries.genres, selectedTvSeries.genres)
-        assertEquals(dummyTvSeries.keywords, selectedTvSeries.keywords)
-        assertEquals(dummyTvSeries.credits, selectedTvSeries.credits)
-        assertEquals(dummyTvSeries.socialLinks, selectedTvSeries.socialLinks)
+    fun getMovieDetail() = testCoroutineRule.runBlockingTest {
+        val idMovie = DataDummy.getMovieDetail().id
+        val resource = Resource.success(DataDummy.getMovieDetail())
+        val data = MutableLiveData<Resource<MovieDetail>>().apply { value = resource }
+        `when`(moviesRepository.getMovieDetailById(idMovie)).thenReturn(data)
+
+        viewModel.initDetail(idMovie, DetailActivity.TYPE_MOVIES)
+        viewModel.movieDetail.observeForever(observerMovieDetail)
+        verify(moviesRepository).getMovieDetailById(idMovie)
+        verify(observerMovieDetail).onChanged(resource)
+        viewModel.movieDetail.removeObserver(observerMovieDetail)
+
+        val movieDetail = viewModel.movieDetail.value
+        Assert.assertNotNull(movieDetail)
+        Assert.assertEquals(resource.data, movieDetail?.data)
     }
 }
