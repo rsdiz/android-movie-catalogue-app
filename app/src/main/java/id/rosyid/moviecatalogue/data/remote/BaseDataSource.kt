23a728dde3accb329b3ce340410d2361cf5f82c9
Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>
2021-05-14 08:36:59 +0700

    add espresso idle
    Signed-off-by: Muhammad Rosyid Izzulkhaq <rosyid.iz.rsd@gmail.com>

--- app/src/main/java/id/rosyid/moviecatalogue/data/remote/BaseDataSource.kt
+++ app/src/main/java/id/rosyid/moviecatalogue/data/remote/BaseDataSource.kt
@@ -1,15 +1,20 @@
 package id.rosyid.moviecatalogue.data.remote
 
+import id.rosyid.moviecatalogue.utils.EspressoIdlingResource
 import id.rosyid.moviecatalogue.utils.Resource
 import retrofit2.Response
 
 abstract class BaseDataSource {
     protected suspend fun <T> getResult(call: suspend () -> Response<T>): Resource<T> {
         try {
+            EspressoIdlingResource.increment()
             val response = call()
             if (response.isSuccessful) {
                 val body = response.body()
-                if (body != null) return Resource.success(body)
+                if (body != null) {
+                    EspressoIdlingResource.decrement()
+                    return Resource.success(body)
+                }
             }
             return error("${response.code()} >> ${response.message()}")
         } catch (e: Exception) {
